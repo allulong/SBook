0243926a2d2f222d50abd4b8ecd4dac84461f209
OurEDA <oureda@172-6-2-4.lightspeed.irvnca.sbcglobal.net>
2017-06-30 18:02:02 +0800

    .

--- app/src/main/java/com/logn/sbook/ui/GuideActivity.java
+++ app/src/main/java/com/logn/sbook/ui/GuideActivity.java
@@ -6,23 +6,32 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.logn.sbook.MainActivity;
 import com.logn.sbook.R;
+import com.logn.sbook.beans.BookInfo;
+import com.logn.sbook.util.BookAdapter;
 import com.shizhefei.view.indicator.FixedIndicatorView;
 import com.shizhefei.view.indicator.IndicatorViewPager;
 import com.shizhefei.view.indicator.transition.OnTransitionTextListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by oureda on 2017/6/29.
  */
 
-public class GuideActivity extends FragmentActivity{
+public class GuideActivity extends FragmentActivity {
     private IndicatorViewPager indicatorViewPager;
     private View centerView;
     private FixedIndicatorView indicator;
@@ -31,10 +40,10 @@ public class GuideActivity extends FragmentActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
-        ViewPager viewPager= (ViewPager) findViewById(R.id.viewPager);
-         indicator= (FixedIndicatorView) findViewById(R.id.indicator);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        indicator = (FixedIndicatorView) findViewById(R.id.indicator);
         indicator.setOnTransitionListener(new OnTransitionTextListener().setColor(Color.CYAN, Color.GRAY));
-        indicatorViewPager=new IndicatorViewPager(indicator,viewPager);
+        indicatorViewPager = new IndicatorViewPager(indicator, viewPager);
 //        inflater=LayoutInflater.from(getApplicationContext());
 
         //这里可以添加一个view，作为centerView，会位于Indicator的tab的中间
@@ -46,6 +55,7 @@ public class GuideActivity extends FragmentActivity{
         // 设置viewpager保留界面不重新加载的页面数量
         viewPager.setOffscreenPageLimit(1);
     }
+
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -56,28 +66,30 @@ public class GuideActivity extends FragmentActivity{
             }
         }
     };
-    private IndicatorViewPager.IndicatorPagerAdapter adapter=new
+    private IndicatorViewPager.IndicatorPagerAdapter adapter = new
             IndicatorViewPager.IndicatorViewPagerAdapter() {
                 private LayoutInflater inflater;
-                public int[] layout={
+                public int[] layout = {
                         R.layout.activity_main,
                         R.layout.activity_mine
                 };
-                private String[] tabNames = {"首页","我"};
-                private int[] tabIcons={
-                        R.drawable.main,R.drawable.mine
+                private String[] tabNames = {"首页", "我"};
+                private int[] tabIcons = {
+                        R.drawable.main, R.drawable.mine
                 };
+
                 @Override
                 public int getCount() {
                     return layout.length;
                 }
 
+
                 @Override
                 public View getViewForTab(int position, View convertView, ViewGroup container) {
-                    if (convertView==null){
-                        inflater=LayoutInflater.from(getApplicationContext());
-                        convertView=inflater.inflate(R.layout.tab_guide
-                        ,container,false);
+                    if (convertView == null) {
+                        inflater = LayoutInflater.from(getApplicationContext());
+                        convertView = inflater.inflate(R.layout.tab_guide
+                                , container, false);
                     }
                     TextView textView = (TextView) convertView;
                     textView.setText(tabNames[position]);
@@ -87,7 +99,7 @@ public class GuideActivity extends FragmentActivity{
 
                 @Override
                 public View getViewForPage(int position, View convertView, ViewGroup container) {
-                    int resourceId=layout[position];
+                    int resourceId = layout[position];
 //                    if (convertView==null){
 //                        convertView=new View(getApplicationContext());
 //                        convertView.setLayoutParams(new ViewGroup.LayoutParams(
@@ -100,16 +112,46 @@ public class GuideActivity extends FragmentActivity{
 //                    }
 //                    return convertView;
                     View view;
-                    if (convertView==null){
-                        view=LayoutInflater.from(getApplicationContext()).inflate(
-                                resourceId,container,false
+                    if (convertView == null) {
 
-                        );
-                    }else {
-                        view=convertView;
+                        view = LayoutInflater.from(getApplicationContext()).inflate(resourceId, container, false);
+
+                        if (resourceId == R.layout.activity_main) {
+                            List<BookInfo> list = new ArrayList<>();
+                            initBook(list);
+                            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+                            Log.e("132", "null???");
+                            LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+                            recyclerView.setLayoutManager(layoutManager);
+                            BookAdapter bookAdapter = new BookAdapter(GuideActivity.this, list);
+                            recyclerView.setAdapter(bookAdapter);
+                        }
+                    } else {
+                        view = convertView;
                     }
                     return view;
 
                 }
             };
+
+    //从后台数据库获取Book数据
+    private void initBook(List<BookInfo> list) {
+        //example for test
+        for (int i = 0; i < 5; i++) {
+            BookInfo bookInfo = new BookInfo();
+            bookInfo.setAuthor("banz" + i);
+            bookInfo.setBookImageId(R.drawable.displayview);
+            bookInfo.setBookName("2222" + i);
+            bookInfo.setDate("2017.6.28");
+            bookInfo.setNewPrice("8.00");
+            bookInfo.setOldPrice("15.00");
+            bookInfo.setQuality("9");
+            bookInfo.setUserAddress("SSDUT");
+            bookInfo.setSexImageId(R.mipmap.ic_launcher);
+            bookInfo.setUserImageId(R.mipmap.ic_launcher);
+            bookInfo.setUserName("banz");
+            list.add(bookInfo);
+        }
+    }
+
 }
