257cb8622a5b8d4ef72e341dd71c2a49483cdcde
OurEDA <oureda@172-6-2-4.lightspeed.irvnca.sbcglobal.net>
2017-07-05 18:52:44 +0800

    .

--- app/src/main/java/com/logn/sbook/ui/AddActivity.java
+++ app/src/main/java/com/logn/sbook/ui/AddActivity.java
@@ -1,17 +1,45 @@
 package com.logn.sbook.ui;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.logn.sbook.R;
 import com.logn.titlebar.TitleBar;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.jar.Manifest;
 
 /**
  * Created by Vivian on 2017/7/1.
@@ -33,6 +61,12 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
     private Button choose_kind;
     private Button choose_qulity;
     TitleBar add_return;
+
+    private static Dialog mCameraDialog;
+
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private static Uri imageUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -94,12 +128,212 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.add_book_image:
+                showDialog(AddActivity.this);
 
                 break;
         }
     }
 
-    //弹出对话框，选择图片获取 
+    //弹出对话框，选择图片获取
+    public void showDialog(Context context) {
+        mCameraDialog = new Dialog(context, R.style.my_dialog);
+        LinearLayout root = (LinearLayout) LayoutInflater.from(context).inflate(
+                R.layout.layout_camera_control, null);
+        root.findViewById(R.id.btn_open_camera).setOnClickListener(btnlistener);
+        root.findViewById(R.id.btn_choose_img).setOnClickListener(btnlistener);
+        root.findViewById(R.id.btn_cancel).setOnClickListener(btnlistener);
+        mCameraDialog.setContentView(root);
+        Window dialogWindow = mCameraDialog.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        dialogWindow.setWindowAnimations(R.style.dialogstyle); // 添加动画
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
+        lp.x = 0; // 新位置X坐标
+        lp.y = -20; // 新位置Y坐标
+        lp.width = (int) getResources().getDisplayMetrics().widthPixels; // 宽度
+//      lp.height = WindowManager.LayoutParams.WRAP_CONTENT; // 高度
+//      lp.alpha = 9f; // 透明度
+        root.measure(0, 0);
+        lp.height = root.getMeasuredHeight();
+        lp.alpha = 9f; // 透明度
+        dialogWindow.setAttributes(lp);
+        mCameraDialog.show();
+    }
+
+    View.OnClickListener btnlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_open_camera: // 打开照相机
+                    takePhoto(AddActivity.this);
+                    if (mCameraDialog != null) {
+                        mCameraDialog.dismiss();
+                    }
+                    break;
+                // 打开相册
+                case R.id.btn_choose_img:
+                    choosePictureFromAlbum(AddActivity.this);
+                    if (mCameraDialog != null) {
+                        mCameraDialog.dismiss();
+                    }
+
+                    break;
+                // 取消
+                case R.id.btn_cancel:
+                    if (mCameraDialog != null) {
+                        mCameraDialog.dismiss();
+                    }
+                    break;
+            }
+        }
+    };
+
+    //打开照相机拍照获取图片
+    public void takePhoto(Context context) {
+        File outputImage = new File(context.getExternalCacheDir(), "output_image.jpg");
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(context, "com.logn.sbook.fileprovider",
+                    outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                                .openInputStream(imageUri));
+                        imageButton_book_look.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    } else {
+                        //4.4及以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    //从相册中选择照片
+    public void choosePictureFromAlbum(Context context) {
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.
+                permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            openAlbum();
+        }
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(AddActivity.this, "You denied the permission",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(AddActivity.this, uri)) {
+            //如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.document".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        selection);
+            } else if ("com.android.providers.downloads.documents".
+                    equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads")
+                        , Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri=data.getData();
+        String imagePath=getImagePath(uri,null);
+        displayImage(imagePath);
+
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path=null;
+        Cursor cursor=getContentResolver().query(uri,null,selection,null,null);
+        if (cursor!=null){
+            if (cursor.moveToFirst()){
+                path=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+
+            }
+            cursor.close();
+        }
+        return path;
+
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath!=null){
+            Bitmap bitmap=BitmapFactory.decodeFile(imagePath);
+            imageButton_book_look.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(AddActivity.this,"failed to get image",Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+    }
+
 }
